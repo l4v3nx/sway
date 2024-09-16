5d8cb56498e65deea9d93ec364027b4c4c5404d9
l4v3nx <ygiki52@gmail.com>
2024-09-16 17:18:00 +0300

    remove broken backlight script

--- .local/bin/sway_backlight_toggle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-read lcd < /tmp/lcd
-    if [ "$lcd" -eq "0" ]; then
-        swaymsg "output * power on"
-        echo 1 > /tmp/lcd
-    else
-        swaymsg "output * power off"
-        echo 0 > /tmp/lcd
-    fi
